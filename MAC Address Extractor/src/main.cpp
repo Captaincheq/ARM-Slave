838b6d6fc25a1f09f56ff6294f4c61f1f302f3b6
Mahadi Sajjad Neloy <sierra117@qq.com>
2022-06-26 22:47:56 +0600

    MAC_Addr
    Processing esp01_1m (platform: espressif8266; board: esp01_1m; framework: arduino)
    -------------------------------------------------------------------------------------------------------------------------------------------------------Verbose mode can be enabled via `-v, --verbose` option
    CONFIGURATION: https://docs.platformio.org/page/boards/espressif8266/esp01_1m.html
    PLATFORM: Espressif 8266 (3.2.0) > Espressif Generic ESP8266 ESP-01 1M
    HARDWARE: ESP8266 80MHz, 80KB RAM, 1MB Flash
    PACKAGES:
     - framework-arduinoespressif8266 @ 3.30002.0 (3.0.2)
     - tool-esptool @ 1.413.0 (4.13)
     - tool-esptoolpy @ 1.30000.201119 (3.0.0)
     - toolchain-xtensa @ 2.100300.210717 (10.3.0)
    LDF: Library Dependency Finder -> https://bit.ly/configure-pio-ldf
    LDF Modes: Finder ~ chain, Compatibility ~ soft
    Found 36 compatible libraries
    Scanning dependencies...
    Dependency Graph
    |-- ESP8266WiFi @ 1.0
    Building in release mode
    Retrieving maximum program size .pio\build\esp01_1m\firmware.elf
    Checking size .pio\build\esp01_1m\firmware.elf
    Advanced Memory Usage is available via "PlatformIO Home > Project Inspect"
    RAM:   [===       ]  34.3% (used 28116 bytes from 81920 bytes)
    Flash: [====      ]  35.0% (used 266857 bytes from 761840 bytes)
    ============================================================= [SUCCESS] Took 2.64 seconds =============================================================

---
+++ MAC Address Extractor/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("MAC Address:");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop()
+{
+}
