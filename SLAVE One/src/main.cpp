699c2d6b5811208cdb72d9ea9b533b6305039805
Mahadi Sajjad Neloy <mahadi.s.neloy@gmail.com>
2023-03-01 11:39:07 +0600

    Update main.cpp

--- SLAVE One/src/main.cpp
+++ SLAVE One/src/main.cpp
@@ -19,9 +19,14 @@ struct_message myData;
 // Callback function that will be executed when data is received
 void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len)
 {
+  // Copy the received data into the myData variable using memcpy
   memcpy(&myData, incomingData, sizeof(myData));
+  
+  // Print the length of the received data
   Serial.print("Bytes received: ");
   Serial.println(len);
+  
+  // Print the values of the different variables in myData
   Serial.print("Char: ");
   Serial.println(myData.a);
   Serial.print("Int: ");
@@ -46,16 +51,22 @@ void setup()
   // Init ESP-NOW
   if (esp_now_init() != 0)
   {
+    // If initialization fails, print an error message and return
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
+  
+  // Set the ESP-NOW role of the device to slave
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  
+  // Register the callback function for receiving data
   esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop()
 {
+  // The loop function is empty as all the work is done in the callback function
 }
